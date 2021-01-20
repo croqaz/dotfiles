221b63081e0f0dcd5ad2174bcf777fc6968d324e
Cristi Constantin <cristi.constantin@sent.com>
2021-01-20 22:21:25 +0000

    Updated dots

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -1,32 +1,5 @@
-extract() {
-  for file in "$@"
-  do
-      if [ -f $file ] ; then
-          ex $file
-      else
-          echo "'$file' is not a valid file"
-      fi
-  done
-}
-
-mkextract() {
-  for file in "$@"
-  do
-      if [ -f $file ] ; then
-          local filename=${file%\.*}
-          mkdir -p $filename
-          cp $file $filename
-          cd $filename
-          ex $file
-          rm -f $file
-          cd -
-      else
-          echo "'$1' is not a valid file"
-      fi
-  done
-}
-
-ex() {
+ext() {
+  # Extract decompress file
   case $1 in
       *.tar.bz2)  tar xjf $1      ;;
       *.tar.gz)   tar xzf $1      ;;
@@ -42,6 +15,34 @@ ex() {
       *.Z)        uncompress $1   ;;
       *)          echo "'$1' cannot be extracted" ;;
   esac
+}
+
+extract() {
+  for file in "$@"
+  do
+      if [ -f $file ] ; then
+          ext $file
+      else
+          echo "'$file' is not a valid file"
+      fi
+  done
+}
+
+mkextract() {
+  for file in "$@"
+  do
+      if [ -f $file ] ; then
+          local filename=${file%\.*}
+          mkdir -p $filename
+          cp $file $filename
+          cd $filename
+          ext $file
+          rm -f $file
+          cd -
+      else
+          echo "'$1' is not a valid file"
+      fi
+  done
 }
 
 pdfextract() {
