2b5a0d73dbefd5636844424e205f718c7a09030c
Cristi Constantin <cristi.constantin@sent.com>
2021-02-12 14:59:12 +0000

    Updated the CPU usage app

--- scripts/cpu_usage/cpu_usage.c
+++ scripts/cpu_usage/cpu_usage.c
@@ -2,7 +2,7 @@
 //
 // Copyright 2021 Cristi Constantin <cristi.constantin@sent.com>
 //
-// Initially based on i3blocks-contrib/cpu_usage2
+// Initially based on i3blocks-contrib/cpu_usage2, but completely re-written
 // Also inspired by procps/proc/sysinfo.c, busybox/procps/top.c
 
 #include <stdio.h>
@@ -10,15 +10,22 @@
 #include <unistd.h>
 #include <getopt.h>
 
-#define WARN_C "#CC241D"
-#define CRIT_C "#D65D0E"
 #define MAX_CPUS 10
 #define MAX_VALU 9.0
 
 typedef unsigned long long int ulli;
 
-struct ProcStat
-{
+char* label = "";
+char* delim = " ";
+
+// Todo: maybe allow user to change them
+const char* warn_c = "#D65D0E";
+const char* crit_c = "#CC241D";
+
+uint warning = (int)(0.75 * MAX_VALU);
+uint critical = (int)(0.9 * MAX_VALU);
+
+struct ProcStat {
     ulli old_used;
     ulli old_total;
     ulli new_used;
@@ -28,74 +35,66 @@ struct ProcStat
 struct ProcStat proc_stats[MAX_CPUS];
 
 const char fmt[] = "cpu%[^ ] %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu";
-// largest CPU line I have seen is 63
-char line_buf[64];
-ulli used, user, nic, sys, idle, iowait, irq, sirq, steal, guest, nguest;
-double usage;
-char scpu;
-uint icpu;
 
-void help(char *argv[])
-{
+void help(char* argv[]) {
     printf("Usage: %s "
-           "[-t seconds] [-w %%age] [-c %%age] [-l label] [-h]\n",
-           argv[0]);
+        "[-t seconds] [-w int] [-c int] [-l label] [-s delim] [-h]\n",
+        argv[0]);
     printf("\n");
-    printf("-t seconds\tSet refresh time (default is 1)\n");
-    printf("-l label\tLabel to print before the cpu usage (default: CPU)\n");
-    printf("-w \t\tSet warning color for CPU usage. (default: %s)\n", WARN_C);
-    printf("-c \t\tSet critical color for CPU usage. (default: %s)\n", CRIT_C);
-    printf("-h \t\tthis help\n");
+    printf("-t seconds\tSet refresh time (default is 1.0)\n");
+    printf("-l label\tLabel to print before the CPU usage (default: '')\n");
+    printf("-s delim\tSeparator to print between CPU numbers (default: ' ')\n");
+    printf("-w \t\tSet warning limit for CPU usage. (default: %u)\n", warning);
+    printf("-c \t\tSet critical limit for CPU usage. (default: %u)\n", critical);
+    printf("-h \t\tThis help\n");
     printf("\n");
 }
 
-void display(const char *label, uint const warning, uint const critical)
-{
+void display() {
+    static uint usage;
     printf("%s<span>", label);
 
-    for (uint i = 0; i < MAX_CPUS; i++)
-    {
-        if (proc_stats[i].old_total < 1)
-        {
+    for (ushort i = 0; i < MAX_CPUS; i++) {
+        if (proc_stats[i].old_total < 1) {
             break;
         }
-        usage = MAX_VALU *
-                (proc_stats[i].new_used - proc_stats[i].old_used) /
-                (proc_stats[i].new_total - proc_stats[i].old_total);
+        usage = (uint)(
+            MAX_VALU *
+            (proc_stats[i].new_used - proc_stats[i].old_used) /
+            (proc_stats[i].new_total - proc_stats[i].old_total)
+            );
 
-        if (i > 0)
-        {
-            // space in between
-            printf(" ");
+        if (i > 0) {
+            printf(delim);
         }
-        if (critical > 0 && usage >= critical)
-        {
-            printf("<span color='%s'>%i=%d</span> ", WARN_C, i, (int)usage);
+
+        if (critical > 0 && usage >= critical) {
+            printf("<span color='%s'>%i=%d</span>", crit_c, i, usage);
         }
-        else if (warning > 0 && usage >= warning)
-        {
-            printf("<span color='%s'>%i=%d</span> ", CRIT_C, i, (int)usage);
+        else if (warning > 0 && usage >= warning) {
+            printf("<span color='%s'>%i=%d</span>", warn_c, i, usage);
         }
-        else
-        {
-            printf("%i=%d", i, (int)usage);
+        else {
+            printf("%i=%d", i, usage);
         }
     }
 
     printf("</span>\n");
 }
 
-void update_cpu_line(FILE *fp)
-{
-    if (!fgets(line_buf, sizeof(line_buf), fp) || line_buf[0] != 'c' /* not "cpu" */)
-    {
+void update_cpu_line(FILE* fp) {
+    static char scpu;
+    static uint icpu;
+    static ulli used, user, nic, sys, idle, iowait, irq, sirq, steal, guest, nguest;
+    // longest CPU line I have seen is 63
+    char line_buf[64];
+    if (!fgets(line_buf, sizeof(line_buf), fp) || line_buf[0] != 'c' /* not "cpu" */) {
         return;
     }
     int ret = sscanf(line_buf, fmt,
-                     &scpu, &user, &nic, &sys, &idle, &iowait, &irq, &sirq, &steal,
-                     &guest, &nguest);
-    if (ret > 10)
-    {
+        &scpu, &user, &nic, &sys, &idle, &iowait, &irq, &sirq, &steal,
+        &guest, &nguest);
+    if (ret > 10) {
         icpu = atoi(&scpu);
         used = user + nic + sys + irq + sirq + steal + guest + nguest;
         proc_stats[icpu].new_used = used;
@@ -104,49 +103,39 @@ void update_cpu_line(FILE *fp)
     return;
 }
 
-void read_proc_stat()
-{
-    FILE *fp = fopen("/proc/stat", "r");
-    if (fp == NULL)
-    {
+void read_proc_stat() {
+    FILE* fp = fopen("/proc/stat", "r");
+    if (fp == NULL) {
         perror("Couldn't open /proc/stat\n");
         exit(EXIT_FAILURE);
     }
-    for (uint i = 0; i < MAX_CPUS + 2; i++)
-    {
+    for (ushort i = 0; i < MAX_CPUS + 2; i++) {
         // the function will only update matching lines
         update_cpu_line(fp);
     }
+    fclose(fp);
     return;
 }
 
-int main(int argc, char *argv[])
-{
-    uint warning = (int)(0.9 * MAX_VALU);
-    uint critical = (int)(0.75 * MAX_VALU);
-    char *label = "";
-    char *envvar = NULL;
-    int c, t = 1;
+inline void sync_new_old_stat() {
+    for (ushort i = 0; i < MAX_CPUS; i++) {
+        proc_stats[i].old_used = proc_stats[i].new_used;
+        proc_stats[i].old_total = proc_stats[i].new_total;
+    }
+}
 
-    envvar = getenv("REFRESH_TIME");
-    if (envvar)
-        t = atoi(envvar);
-    envvar = getenv("WARN_PERCENT");
-    if (envvar)
-        warning = atoi(envvar);
-    envvar = getenv("CRIT_PERCENT");
-    if (envvar)
-        critical = atoi(envvar);
-    envvar = getenv("LABEL");
-    if (envvar)
-        label = envvar;
+int main(int argc, char* argv[]) {
+    const uint m = 1000.0 * 1000.0;
+    float t = 1.0;
+    int c;
 
-    while (c = getopt(argc, argv, "ht:w:c:d:l:"), c != -1)
-    {
-        switch (c)
-        {
+    while (c = getopt(argc, argv, "ht:w:c:l:s:"), c != -1) {
+        switch (c) {
+        case 'h':
+            help(argv);
+            return EXIT_SUCCESS;
         case 't':
-            t = atoi(optarg);
+            t = atof(optarg);
             break;
         case 'w':
             warning = atoi(optarg);
@@ -157,33 +146,23 @@ int main(int argc, char *argv[])
         case 'l':
             label = optarg;
             break;
-        case 'h':
-            help(argv);
-            return EXIT_SUCCESS;
+        case 's':
+            delim = optarg;
+            break;
         }
     }
 
-    // printf("Label=%s, W=%d, C=%d\n", label, warning, critical);
+    printf("Label=%s, Delim=%s, T=%f, W=%d, C=%d\n", label, delim, t, warning, critical);
 
     read_proc_stat();
-    for (uint i = 0; i < MAX_CPUS; i++)
-    {
-        proc_stats[i].old_used = proc_stats[i].new_used;
-        proc_stats[i].old_total = proc_stats[i].new_total;
-    }
+    sync_new_old_stat();
 
-    while (1)
-    {
-        sleep(t);
+    while (1) {
+        usleep(t * m);
         read_proc_stat();
-        display(label, warning, critical);
+        display();
+        sync_new_old_stat();
         fflush(stdout);
-
-        for (uint i = 0; i < MAX_CPUS; i++)
-        {
-            proc_stats[i].old_used = proc_stats[i].new_used;
-            proc_stats[i].old_total = proc_stats[i].new_total;
-        }
     }
 
     return EXIT_SUCCESS;
